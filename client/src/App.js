8a17f61f2df81099523995c46214be42cfa29c2a
Anna Eklöv <annaeklov90@gmail.com>
2021-01-19 20:13:38 +0100

    get user from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 
 function App() {
-  return <div className="wrapper" style={{ padding: "20px" }}></div>;
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    // kolla om det finns _id i localStorage
+    // OM JA, redirecta till /profil och kolla _id i localStorage och ta den infon
+    return () => {};
+  }, []);
+
+  return (
+    <div className="wrapper" style={{ padding: "20px" }}>
+      <Login userInfo={userInfo} setUserInfo={setUserInfo} />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,67 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-function Login() {
+function Login({ setUserInfo }) {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  function postAxios() {
+    axios
+      .post("http://localhost:3000/login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data.data);
+        // när jag fått något här ska jag spara _id i localStorage
+        // OCH redirecta till /something
+        setUserInfo(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (username.trim() === "" || password.trim() === "") {
+      return console.log("tomma inputs");
+    }
+    postAxios();
+  }
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+  }
+  function onChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          required
+          onChange={onChangeUsername}
+          autoFocus
+          value={username}
+        />
+        <input
+          type="password"
+          required
+          name="password"
+          placeholder="Password"
+          minLength="1"
+          onChange={onChangePassword}
+          value={password}
+        />
+        <input type="submit" value="Logga in" />
+      </form>
+    </div>
   );
 }
 
