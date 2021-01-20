05cf87362de52aa2c15a96440871d3f029c406b7
Anna Eklöv <annaeklov90@gmail.com>
2021-01-20 17:37:45 +0100

    structure in mongo db. endpoints done

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 const mongo = require("mongodb");
 const MongoClient = mongo.MongoClient;
 const ObjectId = mongo.ObjectId;
+
 // Connection URL
 // Ska ofta (alltid) vara denna url
 const url = "mongodb://localhost:27017";
@@ -14,8 +15,8 @@ let db = client.db(DBname);
 
 /* Funktionerna ska göras här, som kallas på med anrop från server/index.js */
 
-async function getUser(user) {
-  console.log("inside")
+async function loginUser(user) {
+  console.log("inside getUser");
   try {
     const result = await db
       .collection("usersCollection")
@@ -27,5 +28,92 @@ async function getUser(user) {
   }
 }
 
-module.exports.getUser = getUser;
+async function getBookclubs() {
+  console.log("inside getBookclubs");
+  try {
+    const result = await db.collection("bookclubsCollection").find().toArray();
 
+    return result;
+  } catch {
+    throw error;
+  }
+}
+
+async function getBookclub(bookclubId) {
+  console.log("inside getBookclub");
+  try {
+    const result = await db
+      .collection("bookclubsCollection")
+      .findOne({ _id: ObjectId(bookclubId) });
+
+    return result;
+  } catch {
+    throw error;
+  }
+}
+
+async function updateBookclubBooksToRead(newBook, bookclubId) {
+  try {
+    const result = await db.collection("bookclubsCollection").updateOne(
+      { _id: ObjectId(bookclubId) },
+      {
+        $push: {
+          booksToRead: newBook,
+        },
+      }
+    );
+    return result;
+  } catch {
+    throw error;
+  }
+}
+
+async function updateBookclubBooksRead(newBook, bookclubId) {
+  try {
+    const result = await db.collection("bookclubsCollection").updateOne(
+      { _id: ObjectId(bookclubId) },
+      {
+        $push: {
+          booksRead: newBook,
+        },
+      }
+    );
+    return result;
+  } catch {
+    throw error;
+  }
+}
+
+module.exports.loginUser = loginUser;
+module.exports.getBookclubs = getBookclubs;
+module.exports.getBookclub = getBookclub;
+module.exports.updateBookclubBooksToRead = updateBookclubBooksToRead;
+module.exports.updateBookclubBooksRead = updateBookclubBooksRead;
+
+
+
+
+//module.exports.getAllBooksInOneBookclub = getAllBooksInOneBookclub;
+//module.exports.postBookToBookClub = postBookToBookClub;
+
+/* async function postBookToBookClub(newBook) {
+  newBook.bookclubId = ObjectId(newBook.bookclubId);
+  try {
+    const result = await db.collection("booksCollection").insertOne(newBook);
+    return { success: true, statusCode: 201 };
+  } catch {
+    throw error;
+  }
+} */
+
+/* async function getAllBooksInOneBookclub(bookclubId) {
+  try {
+    const result = await db
+      .collection("booksCollection")
+      .find({ bookclubId: ObjectId(bookclubId) })
+      .toArray();
+    return result;
+  } catch {
+    throw error;
+  }
+} */
--- server/index.js
+++ server/index.js
@@ -14,34 +14,116 @@ app.use(bodyParser.json());
 
 //db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+// Anropen till db.js görs här
+
 app.post("/login", async (req, res) => {
   let user = req.body;
   if (!user.username.length && !user.password.length) {
     res.status(400).end();
   }
-  const data = await db.getUser(user);
+  const data = await db.loginUser(user);
   console.log("data", data);
 
   if (data.password === user.password) {
     res.status(201).send({ data });
   } else {
     res.status(400).end("Wrong password");
-  } 
+  }
 });
 
-// Anropen till db.js görs här, ex:
-
-/*  app.get("/lists", async (req, res) => {
-  const data = await db.getAllLists();
+app.get("/bookclubs", async (req, res) => {
+  const data = await db.getBookclubs();
   if (data) {
-    res.status(200).send(data); // här är svaret, alltså alla listor
+    res.status(200).send(data);
   } else {
     res.status(500).end();
   }
-}); */
+});
+
+app.get("/bookclub/:bookclubId", async (req, res) => {
+  let bookclubId = req.params.bookclubId;
+  const data = await db.getBookclub(bookclubId);
+  if (data) {
+    res.status(200).send(data);
+  } else {
+    res.status(500).end();
+  }
+});
+
+app.put("/addBookToRead/:bookclubId", async (req, res) => {
+  let bookclubId = req.params.bookclubId;
+  console.log(bookclubId);
+
+  if (!req.body.name) {
+    return res.status(400).end();
+  }
+
+  // kommer vara newBook = req.body.data, är ett objekt som kommer från frontend
+  let newBook = {
+    title: "sagan om ringen",
+    author: "jag",
+    genre: "",
+    coverSrc: "",
+    id: "id från API",
+  };
+  const data = await db.updateBookclubBooksToRead(newBook, bookclubId);
+  res.status(200).send("Sucess");
+});
+
+app.put("/addBookRead/:bookclubId", async (req, res) => {
+  let bookclubId = req.params.bookclubId;
+  console.log(bookclubId);
+
+  if (!req.body.name) {
+    return res.status(400).end();
+  }
+
+  // kommer vara newBook = req.body.data, är ett objekt som kommer från frontend
+  let newBook = {
+    name: "sagan om ringen",
+    author: "jag",
+    genre: "",
+    coverSrc: "",
+    id: "id från API",
+    reviews: [{ username: "", comment: "", rating: 0 }],
+  };
+  const data = await db.updateBookclubBooksRead(newBook, bookclubId);
+  res.status(200).send("Sucess");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+
+//----
+
+/* app.post("/addBook/:bookclubId", async (req, res) => {
+  let bookclubId = req.params.bookclubId;
+  console.log(bookclubId);
+
+  if (!req.body.title) {
+    return res.status(400).end();
+  }
+
+  let newBook = {
+    title: "ny bok",
+    author: "pelle",
+    coverSrc: "",
+    genre: "Roman",
+    bookclubId: bookclubId,
+  };
+  const data = await db.postBookToBookClub(newBook, bookclubId);
+  res.status(200).send("Sucess");
+}); */
+
+/* app.get("/books/:bookclubId", async (req, res) => {
+  let bookclubId = req.params.bookclubId;
+  const data = await db.getAllBooksInOneBookclub(bookclubId);
+  if (data) {
+    res.status(200).send(data);
+  } else {
+    res.status(500).end();
+  }
+}); */
